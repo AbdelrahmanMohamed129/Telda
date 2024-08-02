622a7f9822301af4629a668d5bef3e2528d65c47
Abdelrahman Ezzat <74915712+AbdelrahmanMohamed129@users.noreply.github.com>
2024-08-02 17:17:15 +0300

    comments

--- scheduler.cpp
+++ scheduler.cpp
@@ -1,5 +1,6 @@
 #include "scheduler.h"
 
+// Initialize the static data member.
 int Job::cnt = 1;
 
 template <typename _Callable, typename... _Args>
@@ -9,6 +10,7 @@ Job::Job(ull arrival_time, ull interval, ull frequency,
         interval{interval},
         frequency{frequency},
         function_{bind(forward<_Callable>(function), forward<_Args>(args)...)} {
+    // Validation that each job should have running time or frequency greater than 1 second.
     if (interval < 1) {
         throw runtime_error("CronJob's interval can't be less than 1 second");
     }
@@ -28,6 +30,8 @@ Scheduler::Scheduler(string filename, string log_filename) {
         istringstream iss(line);
         ull arrival_time, interval, frequency;
         iss >> arrival_time >> interval >> frequency;
+        // On reading each line in the file make a new job with the info given in the file and make a call back function that returns a 
+        // randomized running time for each function where it takes the given interval and adds a random number from -2 to 2.
         scheduled_jobs.push({arrival_time, std::make_shared<Job>(arrival_time, interval, frequency, [interval]() -> ull {
             return max(1ull, interval + (1ull)*(rand() % 5 - 2));
         })});
@@ -36,13 +40,17 @@ Scheduler::Scheduler(string filename, string log_filename) {
 }
 
 void Scheduler::run(ull time) {
+    // Check for all the completed jobs if they finished then we should log in the logging file.
     while(!completed_jobs.empty() && completed_jobs.top().first <= time) {
         // log that the job is completed
         auto job = completed_jobs.top().second;
         completed_jobs.pop();
         log << "Job " << job->UID << " completed at " << time << endl;
     }
+    // jobs_to_run is a temporary vector that holds all the new Jobs that will go from the scheduled_jobs to completed_jobs.
     vector<shared_ptr<Job>> jobs_to_run;
+    // For each new time check the top scheduled job if its time to run is now then it is moved to the jobs_to_run and then
+    // requeued in the scheduled_jobs with current time + frequency.
     while(!scheduled_jobs.empty() && scheduled_jobs.top().first == time) {
         auto job = scheduled_jobs.top().second;
         if(job->first_run) {
@@ -54,13 +62,18 @@ void Scheduler::run(ull time) {
         jobs_to_run.push_back(job);
     }
 
+    // Run all the jobs in parallel to ensure concurrency
 #pragma omp parallel for
     for (auto job : jobs_to_run) {
+        // Making the logging in here in a critical section to make sure that each job logs itself and don't overwrite any other job.
 #pragma omp critical
         {
             log << "Job " << job->UID << " started running at " << time << endl;
         }
+        // Running the actual function in each Job.
         ull actual_interval= job->function_();
+        // Pushing the job in the completed_jobs with the time it will finish, also making sure that this is done in critical section
+        // to make sure that each jobs is queued without overwriting other jobs.
 #pragma omp critical
         {
             completed_jobs.push({time + actual_interval, job});
--- scheduler.h
+++ scheduler.h
@@ -13,10 +13,16 @@ class Job {
     ull interval;
     ull frequency;
     bool first_run = true;
+
+    // Function pointer to pass a call back function while creating a job for the first time. 
     function<ull()> function_;
+    // using a friend class to make Scheduler access the private data members of Job.
     friend class Scheduler;
 public:
+    // Count the is used to give each new Job a unique UID.
     static int cnt;
+
+    // Constructor to create a Job with a function pointer.
     template <typename _Callable, typename... _Args>
     Job(ull arrival_time, ull interval, ull frequency,
                  _Callable&& function, _Args&& ... args);
@@ -25,10 +31,14 @@ public:
 class Scheduler {
 //    map<ull, vector<Job>> scheduled_jobs;
 //    map<ull, vector<Job>> running_jobs;
+    // Pri Q that holds a pair of arrival time and a Job pointer
     priority_queue<pair<ull, shared_ptr<Job>> , vector<pair<ull, shared_ptr<Job>>>, greater<pair<ull, shared_ptr<Job>>>> scheduled_jobs;
+    // Pri Q that holds a pair of completion time and a Job pointer
     priority_queue<pair<ull, shared_ptr<Job>> , vector<pair<ull, shared_ptr<Job>>>, greater<pair<ull, shared_ptr<Job>>>> completed_jobs;
+    // Holds the logging to print in the output file
     ofstream log;
 public:
+    // Use a default constructor.
     Scheduler() = default;
     // Copy constructor and assignment operator are deleted
     Scheduler(const Scheduler&) = delete;
@@ -37,7 +47,9 @@ public:
     Scheduler(Scheduler&&) = delete;
     Scheduler& operator=(Scheduler&&) = delete;
 
+    // Constructor that takes the input file name and the log file name.
     Scheduler(string filename, string log_filename = "log.txt");
+    // The main function that simulates the scheduler.
     void run(ull time);
 };
 
